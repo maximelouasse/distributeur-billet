3f250d8e687f9e692e3482b61edad051a80be8f7
Maxime <maxlou25@gmail.com>
2018-09-05 11:42:05 +0200

    Gestion solde + ticket

--- js/main.js
+++ js/main.js
@@ -1,45 +1,78 @@
-const montants = [20, 30, 40, 50, 60, 80, 100];
-const montantsButton = document.getElementById('montants');
-let compteUser = undefined;
+/*
+VARIABLES
+*/
+    const montants = [20, 30, 40, 50, 60, 80, 100];
+    const montantsButton = document.getElementById('montants');
+    let compteUser = undefined;
+    let jsonComptesUser = [
+            {
+                "id_compte": 111111,
+                "nom": "Venet",
+                "prenom": "Alexandre",
+                "solde": 1000,
+                "num_carte": 23658412548965742,
+                "code_cb": 1234
+            },
+            {
+                "id_compte": 222222,
+                "nom": "Louasse",
+                "prenom": "Maxime",
+                "solde": 20000,
+                "num_carte": 462514583965214587,
+                "code_cb": 2233
+            },
+            {
+                "id_compte": 333333,
+                "nom": "Houvin",
+                "prenom": "Corentin",
+                "solde": 1,
+                "num_carte": 45874125456325412,
+                "code_cb": 4321
+            }
+    ];
+//
 
-const getAllComptes = async () => {
-    const apiResult = await fetch(`./distriBillet.json`);
-    const jsonResult = await apiResult.json();
-    return jsonResult.comptes;
-}
+/*
+METHODES*/
+    const getAllComptes = async () => {
+        const apiResult = await fetch(`./distriBillet.json`);
+        const jsonResult = await apiResult.json();
+        return jsonResult.comptes;
+    }
 
-const getUser = async (code_cb) => {
-    const comptes = await getAllComptes();
-    for(var compte in comptes)
-    {
-        if(code_cb == comptes[compte].code_cb)
+    const getUser = async (code_cb) => {
+        const comptes = await getAllComptes();
+        for(var compte in jsonComptesUser)
         {
-            return comptes[compte];
+            if(code_cb == comptes[compte].code_cb)
+            {
+                return comptes[compte];
+            }
         }
     }
-}
 
-const displayMontants = () => {
-    for(let i = 0; i < montants.length; i++)
-    {
-        montantsButton.innerHTML += `<button type="button" class="montant" value="${montants[i]}">${montants[i]}€</button>`;
+    const displayMontants = () => {
+        for(let i = 0; i < montants.length; i++)
+        {
+            montantsButton.innerHTML += `<button type="button" class="montant" value="${montants[i]}">${montants[i]}€</button>`;
+        }
+        montantsButton.innerHTML += `<button type="button" class="montant" value="0">Choisir le montant</button>`;
     }
-    montantsButton.innerHTML += `<button type="button" class="montant" value="0">Choisir le montant</button>`;
-}
 
-const checkSolde = (montant) => {
-    if(compteUser != undefined)
-    {
-        if(compteUser.solde >= montant)
+    const checkSolde = (montant) => {
+        if(compteUser != undefined)
         {
-            alert('ok');
-        }
-        else
-        {
-            alert('pas ok');
+            if(compteUser.solde >= montant)
+            {
+                compteUser.solde = compteUser.solde - montant;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
-}
+//
 
 /*
 Lancer l'application une fois la page chargée
@@ -47,7 +80,7 @@ Lancer l'application une fois la page chargée
 window.addEventListener( 'load', async () => {
     // Charger le contenu de la page
     let user = prompt("Saisir votre code : ");
-
+    
     if(user != "")
     {
         let promise = Promise.resolve(getUser(user));
@@ -62,21 +95,42 @@ window.addEventListener( 'load', async () => {
                 //event
                 let montantsButton = document.querySelectorAll('.montant');
                 
+                
                 for(let i = 0; i < montantsButton.length; i++)
                 {
                     montantsButton[i].addEventListener('click', () => {
+                        let solde = undefined;
+                        let montant = 0;
                         if(montantsButton[i].value == 0)
                         {
-                            let montant = prompt("Saisir un montant : ");
+                            montant = prompt("Saisir un montant : ");
 
                             if(montant != "")
                             {
-                                checkSolde(montant);
+                                solde = checkSolde(montant);
                             }
                         }
                         else
                         {
-                            checkSolde(montantsButton[i].value);
+                            solde = checkSolde(montantsButton[i].value);
+                        }
+
+                        if(solde != false)
+                        {
+                            console.log(`Votre compte a un solde de ${compteUser.solde}€`);
+                            
+                            if(confirm("Voulez-vous un ticket de reçu ?"))
+                            {
+                                console.log("Et la nature vous y avez pensé ?");
+                            }
+                            else
+                            {
+                                console.log("C'est bien vous pensez à la nature :) ");
+                            }
+                        }
+                        else
+                        {
+                            console.log(`Le solde de votre comtpte n'est pas suffisant pour retirer ${montant}€`);
                         }
                     });
                 }
